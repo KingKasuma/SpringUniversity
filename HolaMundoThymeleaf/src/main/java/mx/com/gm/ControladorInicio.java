06cea164e6f404d6e15fd0fcdab0cc897e739ac0
Mike <lanselot777hailander@gmail.com>
2020-07-17 08:09:06 -0400

    Iteration of list on thymeleaf

--- HolaMundoThymeleaf/src/main/java/mx/com/gm/ControladorInicio.java
+++ HolaMundoThymeleaf/src/main/java/mx/com/gm/ControladorInicio.java
@@ -1,5 +1,8 @@
 package mx.com.gm;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import lombok.extern.slf4j.Slf4j;
 import mx.com.gm.domain.Persona;
 import org.springframework.beans.factory.annotation.Value;
@@ -17,15 +20,31 @@ public class ControladorInicio {
     @GetMapping("/")
     public String inicio(Model model){
         var mensaje = "Hola Mundo con Thymeleaf";
+        
         var persona = new Persona();
         persona.setNombre("Juan");
         persona.setApellido("Perez");
         persona.setEmail("jperez@mail.com");
         persona.setTelefono("55443322");
+        
+        var persona2 = new Persona();
+        persona2.setNombre("Carla");
+        persona2.setApellido("Gomez");
+        persona2.setEmail("kgomez@mail.com");
+        persona2.setTelefono("123456798");
+        
+        //List<Persona> personas = new ArrayList();
+        //personas.add(persona);
+        //personas.add(persona2);
+        
+        var personas = Arrays.asList(persona, persona2);
+        //var personas = new ArrayList();
+        
         log.info("ejecutando el controlador Spring MVC");
         model.addAttribute("mensaje", mensaje);
         model.addAttribute("saludo", saludo);
-        model.addAttribute("persona", persona);
+        //model.addAttribute("persona", persona);
+        model.addAttribute("personas", personas);
         return "index";
     }
     

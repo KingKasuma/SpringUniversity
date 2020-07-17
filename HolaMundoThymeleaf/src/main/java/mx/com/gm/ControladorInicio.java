e0ba5a4b457f203de43bfde9707fd177268cb559
Mike <lanselot777hailander@gmail.com>
2020-07-16 23:15:15 -0400

    Giving data to view from controller with model

--- HolaMundoThymeleaf/src/main/java/mx/com/gm/ControladorInicio.java
+++ HolaMundoThymeleaf/src/main/java/mx/com/gm/ControladorInicio.java
@@ -1,6 +1,7 @@
 package mx.com.gm;
 
 import lombok.extern.slf4j.Slf4j;
+import mx.com.gm.domain.Persona;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -16,9 +17,15 @@ public class ControladorInicio {
     @GetMapping("/")
     public String inicio(Model model){
         var mensaje = "Hola Mundo con Thymeleaf";
+        var persona = new Persona();
+        persona.setNombre("Juan");
+        persona.setApellido("Perez");
+        persona.setEmail("jperez@mail.com");
+        persona.setTelefono("55443322");
         log.info("ejecutando el controlador Spring MVC");
         model.addAttribute("mensaje", mensaje);
         model.addAttribute("saludo", saludo);
+        model.addAttribute("persona", persona);
         return "index";
     }
     

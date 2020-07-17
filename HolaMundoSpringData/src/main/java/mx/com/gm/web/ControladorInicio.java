7db00d9531f229cbef9e14e09020e13817fc36ae
Mike <lanselot777hailander@gmail.com>
2020-07-17 09:14:58 -0400

    some changes

--- HolaMundoSpringData/src/main/java/mx/com/gm/web/ControladorInicio.java
+++ HolaMundoSpringData/src/main/java/mx/com/gm/web/ControladorInicio.java
@@ -1,4 +1,4 @@
-package mx.com.gm;
+package mx.com.gm.web;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -13,38 +13,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 @Slf4j
 public class ControladorInicio {
-    //Insertando valor que se declaro en el application.properties
-    @Value("${index.saludo}")
-    private String saludo;
-    
+       
     @GetMapping("/")
-    public String inicio(Model model){
-        var mensaje = "Hola Mundo con Thymeleaf";
-        
-        var persona = new Persona();
-        persona.setNombre("Juan");
-        persona.setApellido("Perez");
-        persona.setEmail("jperez@mail.com");
-        persona.setTelefono("55443322");
-        
-        var persona2 = new Persona();
-        persona2.setNombre("Carla");
-        persona2.setApellido("Gomez");
-        persona2.setEmail("kgomez@mail.com");
-        persona2.setTelefono("123456798");
-        
-        //List<Persona> personas = new ArrayList();
-        //personas.add(persona);
-        //personas.add(persona2);
-        
-        var personas = Arrays.asList(persona, persona2);
-        //var personas = new ArrayList();
-        
-        log.info("ejecutando el controlador Spring MVC");
-        model.addAttribute("mensaje", mensaje);
-        model.addAttribute("saludo", saludo);
-        //model.addAttribute("persona", persona);
-        model.addAttribute("personas", personas);
+    public String inicio(Model model){        
+               
+        log.info("ejecutando el controlador Spring MVC");        
+        //model.addAttribute("personas", personas);
         return "index";
     }
     
